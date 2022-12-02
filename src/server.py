0d3da1ae43d39a875734bca7407e2f072f6adefd
Lucy Zhang <lz3744@rit.edu>
2022-12-02 11:35:01 -0500

    added books resource

--- src/server.py
+++ src/server.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_restful import Resource, Api
 from api.hello_world import HelloWorld
 from api.users import Users
+from api.books import Books
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(Users, '/users')
+api.add_resource(Books, '/books')
 
 if __name__ == '__main__':
     app.run(debug=True)

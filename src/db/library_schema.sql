d556149bc189011ef3b3ab0c6cc9c1d37b274fd4
Lucy Zhang <lz3744@rit.edu>
2022-12-02 11:33:44 -0500

    adjusted schema

--- src/db/library_schema.sql
+++ src/db/library_schema.sql
@@ -64,13 +64,14 @@ INSERT INTO users(name, contact_info) VALUES
     ('Art Garfunkel', 'AGarfunkel@gmail.com');
 
 INSERT INTO inventory(title, book_type, author, publish_date, summary, copies) VALUES
-    ('Figuring', 'Non-fiction', 'Maria Popova', 2019, 'Explores the complexities
-        of love and the human search for truth and meaning', 5),
-    ('In Defence of Witches', 'Non-fiction', 'Mona Chollet', 2022, 'Explores how
-        women who assert their powers are too often seen as a threat to men and society', 3),
-    ('Scary Smart', 'Non-fiction', 'Mo Gawdat', 2021, 'Explores the future of artificial intelligence', 7),
-    ('The Princess Spy', 'Non-fiction', 'Larry Loftis', 2022, 'Follows the hidden history 
-        of an ordinary American girl who became one of the most daring WWII spies', 2),
+    ('Figuring', 'Non-fiction', 'Maria Popova', 2019,
+        'Explores the complexities of love and the human search for truth and meaning', 5),
+    ('In Defence of Witches', 'Non-fiction', 'Mona Chollet', 2022,
+        'Explores how women who assert their powers are too often seen as a threat to men and society', 3),
+    ('Scary Smart', 'Non-fiction', 'Mo Gawdat', 2021,
+        'Explores the future of artificial intelligence', 7),
+    ('The Princess Spy', 'Non-fiction', 'Larry Loftis', 2022,
+        'Follows the hidden history of an ordinary American girl who became one of the most daring WWII spies', 2),
     ('The Dead Romantics', 'Fiction', 'Ashley Poston', 2022,
         'The main character is a ghostwriter for a romance novelist', 6),
     ('The Lord of the Rings', 'Fiction', 'J.R.R. Tolkien', 1954, 
@@ -81,7 +82,8 @@ INSERT INTO inventory(title, book_type, author, publish_date, summary, copies) V
         'Chronicles the childhood of Scout and Jem Finch', 1),
     ('Frankenstein', 'Fiction', 'Mary Shelley', 1818,
         'A young scientist who creates a sapient creature in an scientific experiment', 1),
-    ('The Winds of Winter', 'Fiction', 'George R.R. Martin', DEFAULT, DEFAULT, 4);
+    ('The Winds of Winter', 'Fiction', 'George R.R. Martin', 2023,
+        'Sixth novel in the epic fantasy series "A Song of Ice and Fire"', 4);
 
 INSERT INTO libraries(library_name) VALUES
     ('Penfield'),

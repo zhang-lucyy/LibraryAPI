9264244c49fbed62ea2093c755a8edb92a1b8b62
Lucy Zhang <lz3744@rit.edu>
2022-12-02 02:21:46 -0500

    added api.users

--- src/server.py
+++ src/server.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from api.hello_world import HelloWorld
+from api.users import Users
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(Users, '/users')
 
 if __name__ == '__main__':
     app.run(debug=True)

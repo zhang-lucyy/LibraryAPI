3a80114913e863f0dce3ccdac9edc92a8c63b959
Lucy Zhang <lz3744@rit.edu>
2022-12-07 01:19:19 -0500

    added user resource

--- src/server.py
+++ src/server.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from api.hello_world import HelloWorld
-from api.users import Users
+from api.users import User, Users
 from api.books import *
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(User, '/user/<user_id>')
 api.add_resource(Users, '/users')
 api.add_resource(Books, '/books')
 api.add_resource(SearchBooksSingleTerm, '/books/<type>')
